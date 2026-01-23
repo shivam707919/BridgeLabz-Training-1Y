4db09c65e7cf89114179e4ca90722c5247dc34b5
shivam707919 <shivamkumartiwari70@gmail.com>
2026-01-23 11:53:32 +0530

    Practice

---
+++ Practice-Program/IfElseExample.java
@@ -0,0 +1,14 @@
+class IfElseExample{
+public static void main(String[] args){ //--main method
+int age=16;
+
+if(age>=17){
+System.out.println("Eligible");
+}
+
+else
+{
+System.out.println("Not Eligible");
+
+}
+}
---
+++ Practice-Program/IfExample.java
@@ -0,0 +1,10 @@
+class IfExample{
+public static void main(String[] args){ //--main method
+int age=20;
+
+if(age>=18){
+System.out.println("Eligible");
+}
+
+}
+}
